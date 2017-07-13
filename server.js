9c2f769c2850604977cfefd393812a5780455d79
sujaykumardas <sdas@grepruby.com>
2017-07-13 17:03:18 +0530

    day6: ajax request and back end validation completed

--- server.js
+++ server.js
@@ -8,17 +8,12 @@ var app = express();
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
-
-
-
-// about page 
 app.get('/about', function(req, res) {
     res.render('pages/about');
 });
 
 app.get('/', function(req, res) {
-    console.log("inside get");
+    //console.log("inside get");
     res.render('pages/ajform');
 });
 
@@ -26,11 +21,17 @@ app.use(bp.urlencoded({ extended: false }));
 app.use(bp.json());
 
 app.post('/', function(req, res) {
-   console.log("inside post");
-   console.log(req.body.fname);
-   //console.log(req.body.obj.fname);      // your JSON
-   //var json = JSON.parse(req.body);
+   
    var str= /^[A-Za-z]+$/;
+   var num= /^[0-9]{2}$/;
+   var yn= /^(y|n){1}$/;
+   var dof= /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-](1[9][0-9][0-9]|2[0][10][0-6])$/;
+   var emid= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+   var ph =/^\d{10}$/;
+   var cgpaf= /^[0-9]{1}\.{1}[0-9]+$/;
+   var pswd= /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
+   var cpat= /^sU8Nki$/i;
+   var pin= /^[0-9]{6}$/;
    var ch=0;
    var obj={};
         if(!str.test(req.body.fname))
@@ -46,6 +47,51 @@ app.post('/', function(req, res) {
         {
            ch=1;
            obj.lname="invalid last name";
+        }
+        if(!ph.test(req.body.phn))
+        {
+           ch=1;
+           obj.phn="invalid";
+        }
+        if(!pswd.test(req.body.pid))
+        {
+           ch=1;
+           obj.pid="invalid";
+        }
+        if(!yn.test(req.body.ms))
+        {
+           ch=1;
+           obj.ms="invalid";
+        }
+        if(!pin.test(req.body.pnc))
+        {
+           ch=1;
+           obj.pnc="invalid";
+        }
+        if(!emid.test(req.body.mid))
+        {
+           ch=1;
+           obj.mid="invalid";
+        }
+        if(!num.test(req.body.age))
+        {
+           ch=1;
+           obj.age="invalid";
+        }
+        if(!cgpaf.test(req.body.gpa))
+        {
+           ch=1;
+           obj.gpa="invalid";
+        }
+        if(!dof.test(req.body.dob))
+        {
+           ch=1;
+           obj.dob="invalid";
+        }
+        if(!cpat.test(req.body.cpt))
+        {
+           ch=1;
+           obj.cpt="invalid";
         }
 
         if(ch==0)
