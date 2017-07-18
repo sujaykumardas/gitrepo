cdfdd811abf7155ea24ac8586e7b1089a037eb31
sujaykumardas <sdas@grepruby.com>
2017-07-18 17:05:55 +0530

    Mongoose Express Connect With Delete

--- mongoose/connect.js
+++ mongoose/connect.js
@@ -44,22 +44,29 @@ app.get('/read', function(req, res) {
     //console.log("inside insert");
 });
 
-app.get('/update', function(req, res) {
-    var data=[];
-    Worker.find({}, function (err, docs) {
+app.get('/update/:id', function(req, res) {
+    // var data=[];
+    var id = req.params.id;
+    console.log(id);
+    Worker.findById(id, function (err, docs) {
            if(err){
             console.log("error while reading");
            }
-           data=docs;
-           console.log(data);
-           docs.forEach(function(u) {
-           //console.log(u);
+           
+           res.render('pages/update', {data: docs});
         });
-           res.render('pages/update',{data:data});
-      });
-    
-    //console.log("inside insert");
 });
+
+app.get('/delete/:id', function(req, res) {
+    // var data=[];
+    var id = req.params.id;
+    console.log(id);
+    var obj={};
+    obj.ide=id;
+    res.render('pages/delete', obj);
+ }); 
+    //console.log("inside insert");
+
 
 app.use(bp.urlencoded({ extended: false }));
 app.use(bp.json());
@@ -126,6 +133,40 @@ app.post('/insert', function(req, res) {
 });   */  
 
 app.post('/update', function(req, res) {
+  //console.log("inside post insert");
+   
+  Worker.findByIdAndUpdate(req.body.ide,{$set:{
+      fname:req.body.fname, 
+      lname:req.body.lname, 
+      age:req.body.age, 
+      password:req.body.pid, 
+      pincode:req.body.pnc, 
+      email:req.body.mid
+       }},function (err, docs) {
+           if(err){
+            console.log("error while updating");
+            obj.inc="2";
+            console.log("not successful"+err);
+           }
+           else{
+            var obj={};
+            obj.cor="1";
+            console.log("success");
+
+           }
+           
+           res.set('Content-Type', 'application/json');
+           console.log(obj);
+           res.send(JSON.stringify(obj));
+           
+           
+      });
+    
+  
+  
+});  
+
+app.post('/delete', function(req, res) {
   //console.log("inside post insert");
    
   Worker.findByIdAndUpdate(req.body.ide,{$set:{
